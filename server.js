b3f4da41da3e44091828a865f9e834163443e0c9
Dan <danzemly000@gmail.com>
2024-08-28 14:46:49 +0300

    updated server

--- server.js
+++ server.js
@@ -1,40 +1,57 @@
+const { error } = require('console');
 const express = require('express');
 const https = require('https');
 
-const baseURL='https://pixabay.com/api/'
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server url is https://localhost:${PORT}`);
 });
 
 const API_KEY = "42079125-1909400cd5615db78c9c2cb93";
+const baseURL = 'pixabay.com'
 
-const getImage = async (req ,res) => {
-let data = {}; //expect object
-try {
-  const response = 
-   https.get(`${baseURL}` , (res) => {
-    if (response.statusCode !== 200) {
-    res.status(response.statusCode).send('Failed to fetch image');
-    return;
-}
+const getImage = async (req, res) => {
 
+    const options = {
+        hostname: baseURL,
+        port: 443,
+        path: '/api/',
+        headers: {
+            Authorization: `Bearer ${API_KEY}`
+        }
+    }
+    let data = []; //hold data here
+    try {
+        https.get(options, (response) => {
+            if (response.statusCode !== 200) {
+                res.status(response.statusCode).send('Failed to fetch image');
+                return;
+            }
 
-res.on('data' , (chunk) => {
-    data = chunk;
-})
-//send data back with success code.
-res.on('end' , () => {res.status(200).send(data)})})
+            //add chunks to the data aray
+            response.on('data', (chunk) => {
+                data.push(chunk);
+            })
+        })
 
+        response.on('end', () => {
+            //combine data chunks and send them back
+            const imageBuffer = Buffer.concat(data);
+            res.status(200).send(imageBuffer);
+        })
+            .on('error', () => {
+                console.error(error);
+                res.status(500).send('An error occurred while fetching the image');
+            })
+    }
 
-}
-
-catch (error) {
-    console.error(error);
-    res.status(500).send('An error occurred while fetching the image');}
+    catch (error) {
+        console.error(error);
+        res.status(500).send('An error occurred while fetching the image');
+    }
 
 }
 
